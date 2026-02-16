cf84d48894dfd50c5c4db733e4c250bf66d86452
Stepan-Telegin <i7805918@gmail.com>
2026-02-16 11:09:27 +0200

    Added data output from the session to index.php

--- code/index.php
+++ code/index.php
@@ -1,2 +1,29 @@
-<?php
-phpinfo();
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Результат</title>
+</head>
+
+<body>
+    <?php if(isset($_SESSION['username'])): ?>
+        <h2>Данные из сессии:</h2>
+        <ul>
+            <li>Имя: <?= $_SESSION['username'] ?></li>
+            <li>Дата рождения: <?= $_SESSION['birthdate'] ?></li>
+            <li>Тема: <?= $_SESSION['topic'] ?></li>
+            <li>Материалы: <?= $_SESSION['materials'] ?></li>
+            <li>Формат: <?= $_SESSION['format'] ?></li>
+        </ul>
+    <?php else: ?>
+        <p>Данных пока нет.</p>
+    <?php endif; ?>
+
+    <br>
+    <a href="form.html">Заполнить форму еще раз</a> | 
+    <a href="view.php">Посмотреть все сохраненные данные</a>
+
+</body>
+</html>
